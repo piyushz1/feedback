56c2ab5b3b0ddf2259b416093d570f012ecd6e40
Pruthvid <pruthvid@zignuts.com>
2024-07-19 10:37:10 +0530

    fix

--- src/app/api/board/route.js
+++ src/app/api/board/route.js
@@ -2,50 +2,50 @@
 
 // const prisma = new PrismaClient();
 
-// export async function GET(req) {
-//   return new Response(JSON.stringify({ message: "Board Deleted" }), {
-//     status: 200,
-//   });
-// }
-// export async function DELETE(req, res) {
-//   const searchParams = req.nextUrl.searchParams;
+export async function GET(req) {
+  return new Response(JSON.stringify({ message: "Board Deleted" }), {
+    status: 200,
+  });
+}
+export async function DELETE(req, res) {
+  const searchParams = req.nextUrl.searchParams;
 
-//   const boardId = searchParams.get("boardId");
+  const boardId = searchParams.get("boardId");
 
-//   if (!boardId) {
-//     return new Response(
-//       JSON.stringify({
-//         message: "board ID is required",
-//       }),
-//       { status: 400 }
-//     );
-//   }
+  if (!boardId) {
+    return new Response(
+      JSON.stringify({
+        message: "board ID is required",
+      }),
+      { status: 400 }
+    );
+  }
 
-//   try {
-//     // Check if the board exists
-//     const board = await prisma.board.findUnique({
-//       where: { id: boardId },
-//     });
+  try {
+    // Check if the board exists
+    const board = await prisma.board.findUnique({
+      where: { id: boardId },
+    });
 
-//     if (!board) {
-//       return new Response(
-//         JSON.stringify({
-//           message: "Board not found",
-//         }),
-//         { status: 404 }
-//       );
-//     }
+    if (!board) {
+      return new Response(
+        JSON.stringify({
+          message: "Board not found",
+        }),
+        { status: 404 }
+      );
+    }
 
-//     // Delete the board
-//     await prisma.board.delete({
-//       where: { id: boardId },
-//     });
-//     return new Response(JSON.stringify({ message: "Board Deleted" }), {
-//       status: 200,
-//     });
-//   } catch (error) {
-//     return new Response(JSON.stringify({ message: "Server Error!" }), {
-//       status: 500,
-//     });
-//   }
-// }
+    // Delete the board
+    await prisma.board.delete({
+      where: { id: boardId },
+    });
+    return new Response(JSON.stringify({ message: "Board Deleted" }), {
+      status: 200,
+    });
+  } catch (error) {
+    return new Response(JSON.stringify({ message: "Server Error!" }), {
+      status: 500,
+    });
+  }
+}
